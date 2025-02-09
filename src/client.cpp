6561d0c9671d28c9dfe72dbdb38914cda16fa4c9
SamakshAgarwal1112 <agarwalsamaksh11@gmail.com>
2025-02-10 01:29:31 +0530

    Added asynchronous connection handling for server

--- src/client.cpp
+++ src/client.cpp
@@ -9,22 +9,26 @@ using std::cout;
 using std::endl;
 
 int main() {
-     boost::asio::io_service io_service;
-     tcp::socket socket(io_service);
-     socket.connect( tcp::endpoint( boost::asio::ip::address::from_string("127.0.0.1"), 8088 ));
-     const string msg = "Hello from Client!\n";
-     boost::system::error_code error;
-     boost::asio::write( socket, boost::asio::buffer(msg), error );
-     if( !error ) {
-        cout << "Client sent hello message!" << endl;
-     }
-     else {
-        cout << "send failed: " << error.message() << endl;
-     }
+    boost::asio::io_service io_service;
+    tcp::socket socket(io_service);
+    socket.connect( tcp::endpoint( boost::asio::ip::address::from_string("127.0.0.1"), 8088 ));
+    string msg = "";
+    cout << "Enter your message: ";
+    getline(cin, msg);
+    msg += "\n";
+
+    boost::system::error_code error;
+    boost::asio::write( socket, boost::asio::buffer(msg), error );
+    if( !error ) {
+        cout << "Client sent message successfully to server!" << endl;
+    }
+    else {
+        cout << "Send failed: " << error.message() << endl;
+    }
     boost::asio::streambuf receive_buffer;
     boost::asio::read(socket, receive_buffer, boost::asio::transfer_all(), error);
     if( error && error != boost::asio::error::eof ) {
-        cout << "receive failed: " << error.message() << endl;
+        cout << "Receive failed: " << error.message() << endl;
     }
     else {
         const char* data = boost::asio::buffer_cast<const char*>(receive_buffer.data());
--- src/server.cpp
+++ src/server.cpp
@@ -6,6 +6,36 @@ using std::string;
 using std::cin;
 using std::cout;
 using std::endl;
+#include "con_handler.cpp"
+
+class Server 
+{
+private:
+   tcp::acceptor acceptor_;
+   void start_accept()
+   {
+    // socket
+     con_handler::pointer connection = con_handler::create(acceptor_.get_io_service());
+
+    // asynchronous accept operation and wait for a new connection.
+     acceptor_.async_accept(connection->socket(),
+        boost::bind(&Server::handle_accept, this, connection,
+        boost::asio::placeholders::error));
+  }
+public:
+//constructor for accepting connection from client
+  Server(boost::asio::io_service& io_service): acceptor_(io_service, tcp::endpoint(tcp::v4(), 8088))
+  {
+     start_accept();
+  }
+  void handle_accept(con_handler::pointer connection, const boost::system::error_code& err)
+  {
+    if (!err) {
+      connection->start();
+    }
+    start_accept();
+  }
+};
 
 string read_(tcp::socket & socket) {
        boost::asio::streambuf buf;
@@ -19,14 +49,24 @@ void send_(tcp::socket & socket, const string& message) {
 }
 
 int main(){
-    boost::asio::io_service io_service;
-    tcp::acceptor acceptor_(io_service, tcp::endpoint(tcp::v4(), 8088 ));
-    tcp::socket socket_(io_service);
-    acceptor_.accept(socket_);
-    string message = read_(socket_);
-    cout << message << endl;
-    send_(socket_, "Hello From Server!");
-    cout << "Servent sent Hello message to Client!" << endl;
-    cout<<"Lol"<<endl;
-    return 0;
+//     boost::asio::io_service io_service;
+//     tcp::acceptor acceptor_(io_service, tcp::endpoint(tcp::v4(), 8088 ));
+//     tcp::socket socket_(io_service);
+//     acceptor_.accept(socket_);
+//     string message = read_(socket_);
+//     cout << message << endl;
+//     send_(socket_, "Hello From Server!");
+//     cout << "Server sent message successfully to Client!" << endl;
+//     return 0;
+  try
+    {
+    boost::asio::io_service io_service;  
+    Server server(io_service);
+    io_service.run();
+    }
+  catch(std::exception& e)
+    {
+    std::cerr << e.what() << endl;
+    }
+  return 0;   
 }
